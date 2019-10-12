72dbb5e0498916ce40b2596c35c5604ba32904ed
David Ferrer <david.ferrer@estudiants.urv.cat>
2019-10-12 02:44:45 +0200

    David: Afegit reconeixement facial

--- app/src/main/java/cat/urv/deim/joanjara/davidferrer/nicolasface/MainActivity.kt
+++ app/src/main/java/cat/urv/deim/joanjara/davidferrer/nicolasface/MainActivity.kt
@@ -4,14 +4,44 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Build.*
 import android.os.Bundle
 import android.widget.Toast
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import android.provider.MediaStore
+import android.graphics.Bitmap
+import android.graphics.Paint
+import android.R.attr.bitmap
+import android.graphics.Rect
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
+
+
+
+
+
+
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    // High-accuracy landmark detection and face classification
+    val highAccuracyOpts = FirebaseVisionFaceDetectorOptions.Builder()
+        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+        .build()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,7 +105,68 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
             image_view.setImageURI(data?.data)
+            detectFaces(data)
+
         }
         super.onActivityResult(requestCode,resultCode,data)
     }
+
+    fun detectFaces(data: Intent?) {
+        val image: FirebaseVisionImage
+
+        try {
+            if(data?.data != null){
+                val workingBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, data.data!!)
+                val mutableBitmap = workingBitmap.copy(workingBitmap.config, true)
+
+                val canvas = Canvas(mutableBitmap)
+
+                val color = -0xbdbdbe
+                val paint = Paint()
+                val rect = Rect(0, 0, mutableBitmap.getWidth(), mutableBitmap.getHeight())
+                paint.setAntiAlias(true);
+                canvas.drawARGB(0, 0, 0, 0);
+                paint.setColor(color);
+
+                image = FirebaseVisionImage.fromFilePath(this, data.data!!)
+
+                // Obtenemos una instancia de FirebaseVisionFaceDetector
+                val detector = FirebaseVision.getInstance()
+                    .getVisionFaceDetector(highAccuracyOpts)
+
+                // Pasamos la imagen al metodo detectInImage
+                val result = detector.detectInImage(image)
+                    .addOnSuccessListener { faces ->
+                        // Task completed successfully
+                        canvas.rotate((-Math.PI/2).toFloat());
+                        for (face in faces) {
+                            var bounds = face.boundingBox
+                            canvas.drawCircle(bounds.exactCenterX(),bounds.exactCenterY(),bounds.height().toFloat()/2,paint)
+
+                            ///Toast.makeText(this,"X= "+face.boundingBox.centerX().toString()+" Y="+face.boundingBox.centerY(),Toast.LENGTH_SHORT).show()
+                        }
+
+                        canvas.drawBitmap(mutableBitmap, rect, rect, paint);
+                        image_view.setImageBitmap(mutableBitmap)
+                        Toast.makeText(this, faces.size.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener(
+                        object : OnFailureListener {
+                            override fun onFailure(e: Exception) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        })
+
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
 }

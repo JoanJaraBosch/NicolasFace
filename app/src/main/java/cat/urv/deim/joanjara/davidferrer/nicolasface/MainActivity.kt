a99b1a2bd22cbd50e2e752ca748baa352ec19b87
David Ferrer <david.ferrer@estudiants.urv.cat>
2019-10-12 03:24:41 +0200

    David: Afegit dibuixat cercles a les cares

--- app/src/main/java/cat/urv/deim/joanjara/davidferrer/nicolasface/MainActivity.kt
+++ app/src/main/java/cat/urv/deim/joanjara/davidferrer/nicolasface/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.os.Build.*
 import android.os.Bundle
 import android.widget.Toast
-import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.ml.vision.FirebaseVision
@@ -15,21 +14,8 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import android.provider.MediaStore
-import android.graphics.Bitmap
-import android.graphics.Paint
-import android.R.attr.bitmap
-import android.graphics.Rect
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
-
-
-
-
+import android.graphics.*
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
 
 
 @Suppress("DEPRECATION")
@@ -50,20 +36,19 @@ class MainActivity : AppCompatActivity() {
         //BUTTON CLICK
         img_pick_btn.setOnClickListener {
             //check runtime permission
-            if (VERSION.SDK_INT >= VERSION_CODES.M){
+            if (VERSION.SDK_INT >= VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED){
+                    PackageManager.PERMISSION_DENIED
+                ) {
                     //permission denied
                     val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
                     //show popup to request runtime permission
                     requestPermissions(permissions, PERMISSION_CODE);
-                }
-                else{
+                } else {
                     //permission already granted
                     pickImageFromGallery();
                 }
-            }
-            else{
+            } else {
                 //system OS is < Marshmallow
                 pickImageFromGallery();
             }
@@ -85,15 +70,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     //handle requested permission result
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
             PERMISSION_CODE -> {
-                if (grantResults.size >0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     //permission from popup granted
                     pickImageFromGallery()
-                }
-                else{
+                } else {
                     //permission from popup denied
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
@@ -103,30 +92,22 @@ class MainActivity : AppCompatActivity() {
 
     //handle result of picked image
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             image_view.setImageURI(data?.data)
             detectFaces(data)
 
         }
-        super.onActivityResult(requestCode,resultCode,data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     fun detectFaces(data: Intent?) {
         val image: FirebaseVisionImage
 
         try {
-            if(data?.data != null){
-                val workingBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, data.data!!)
-                val mutableBitmap = workingBitmap.copy(workingBitmap.config, true)
+            if (data?.data != null) {
+                val workingBitmap =
+                    MediaStore.Images.Media.getBitmap(this.contentResolver, data.data!!)
 
-                val canvas = Canvas(mutableBitmap)
-
-                val color = -0xbdbdbe
-                val paint = Paint()
-                val rect = Rect(0, 0, mutableBitmap.getWidth(), mutableBitmap.getHeight())
-                paint.setAntiAlias(true);
-                canvas.drawARGB(0, 0, 0, 0);
-                paint.setColor(color);
 
                 image = FirebaseVisionImage.fromFilePath(this, data.data!!)
 
@@ -138,16 +119,10 @@ class MainActivity : AppCompatActivity() {
                 val result = detector.detectInImage(image)
                     .addOnSuccessListener { faces ->
                         // Task completed successfully
-                        canvas.rotate((-Math.PI/2).toFloat());
-                        for (face in faces) {
-                            var bounds = face.boundingBox
-                            canvas.drawCircle(bounds.exactCenterX(),bounds.exactCenterY(),bounds.height().toFloat()/2,paint)
+                        //canvas.rotate((-Math.PI / 2).toFloat());
+                        modifyFaces(faces,workingBitmap)
 
-                            ///Toast.makeText(this,"X= "+face.boundingBox.centerX().toString()+" Y="+face.boundingBox.centerY(),Toast.LENGTH_SHORT).show()
-                        }
 
-                        canvas.drawBitmap(mutableBitmap, rect, rect, paint);
-                        image_view.setImageBitmap(mutableBitmap)
                         Toast.makeText(this, faces.size.toString(), Toast.LENGTH_SHORT).show()
                     }
                     .addOnFailureListener(
@@ -162,11 +137,46 @@ class MainActivity : AppCompatActivity() {
 
         } catch (e: Exception) {
             e.printStackTrace()
-            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
         }
     }
 
+    fun modifyFaces(list : List<FirebaseVisionFace>, bitmap: Bitmap){
+        var mutableBitmap = bitmap.copy(bitmap.config, true)
+        val matrix = Matrix()
 
+        matrix.postRotate(-90F)
 
+        //val scaledBitmap = Bitmap.createScaledBitmap(bitmapOrg, width, height, true)
+
+        mutableBitmap = Bitmap.createBitmap(
+            mutableBitmap, 0,
+            0,
+            mutableBitmap.width,
+            mutableBitmap.height,
+            matrix,
+            true
+        )
+        val canvas = Canvas(mutableBitmap)
+
+        val color = -0xbdbdbe
+        val paint = Paint()
+        val rect = Rect(0, 0, mutableBitmap.getWidth(), mutableBitmap.getHeight())
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+
+        for (face in list) {
+            var bounds = face.boundingBox
+            canvas.drawCircle(
+                bounds.exactCenterX(),
+                bounds.exactCenterY(),
+                bounds.height().toFloat() / 2,
+                paint
+            )
+        }
+        canvas.drawBitmap(mutableBitmap, rect, rect, paint);
+        image_view.setImageBitmap(mutableBitmap)
+    }
 
 }
